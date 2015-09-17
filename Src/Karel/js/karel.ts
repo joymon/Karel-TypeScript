9438608219933188824fb0854bb475e920892a63
joymon <joymon@gmail.com>
2015-09-17 15:11:15 -0400

    Refactoring - Added IKarelActions

--- Src/Karel/js/karel.ts
+++ Src/Karel/js/karel.ts
@@ -1,4 +1,6 @@
 /// <reference path="kareldrawer.ts" />
+/// <reference path="moveaction.ts" />
+/// <reference path="turlleftaction.ts" />
 /// <reference path="world.ts" />
 class Karel {
     element: HTMLCanvasElement;
@@ -13,7 +15,6 @@ class Karel {
         this.context = this.element.getContext("2d");
         this.world = world;
         this.drawer = new KarelDrawer(this.context,this.world);
-        
         this.draw();
     }
 
@@ -21,46 +22,13 @@ class Karel {
         this.drawer.draw(this.row, this.col, this.direction);
     }
     move() {
-        var worldSize = this.world.getSize();
-        switch (this.direction) {
-            case Direction.North:
-                if (this.row === 0) {
-                    alert("cannot move");
-                } else {
-                    this.row = this.row - 1;
-                }
-                break;
-            case Direction.West:
-                if (this.col === 0) {
-                    alert("cannot move");
-                } else {
-                    this.col = this.col - 1;
-                }
-                break;
-            case Direction.South:
-                if (this.row === worldSize.rows - 1) {
-                    alert("cannot move");
-                }else {
-                    this.row = this.row + 1;
-                }
-                break;
-            case Direction.East:
-                if (this.col === worldSize.cols - 1) {
-                    alert("cannot move");
-                } else {
-                    this.col = this.col + 1;
-                }
-                break;
-        }
+        var action = new MoveAction();
+        action.Execute(this);
         setTimeout(() => this.draw(), 1000);
     }
     turnLeft() {
-        switch (this.direction) {
-            case Direction.North: this.direction = Direction.West; break;
-            case Direction.West: this.direction = Direction.South; break;
-            case Direction.South: this.direction = Direction.East; break;
-            case Direction.East: this.direction = Direction.North; break;
-        }
+        var action = new TurnLeftAction();
+        action.Execute(this);
         setTimeout(() => this.draw(), 1000);
     }
     
